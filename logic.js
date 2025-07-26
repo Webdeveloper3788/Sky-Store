a4a453230aade275a66a7852e4bde92c2159c2c0
Rana Hashir <itshashirrajpoot@gmail.com>
2025-07-26 14:30:41 +0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,15 @@
+let val = document.querySelector('.buy-button')
+console.log(val)
+
+val.onclick = ( ) => {
+alert('Product added in cart')
+ }
+
+ function openSidebar() {
+  document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeSidebar() {
+  document.getElementById("sidebar").style.width = "0";
+}
+
